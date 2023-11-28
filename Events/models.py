a15c00723c46a4beb94d57aa775ea1319672fc07
MdAshiqurRahmanZayed <mdzayed2019@gmail.com>
2023-11-28 09:49:28 +0600

    Registration For Event model

--- Events/models.py
+++ Events/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from Accounts.models import Account
 # Create your models here.
 class Event(models.Model):
     title = models.CharField(max_length=200)
@@ -8,3 +8,7 @@ class Event(models.Model):
     time = models.TimeField()
     location = models.CharField(max_length=200)
     slots_available = models.IntegerField(default=0)
+    
+class RegistrationForEvent(models.Model):
+    user  = models.ForeignKey(Account, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE) 
